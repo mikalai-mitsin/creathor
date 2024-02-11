91ccbe4bbdd614610dd49e80ecbaa3512bb25155
018bf <>
2024-02-11 19:57:07 +0100

    fix: generate user from domain

---
+++ internal/generators/user/usecases/permission.go
@@ -0,0 +1,227 @@
+package usecases
+
+import (
+	"bytes"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/printer"
+	"go/token"
+	"os"
+	"path"
+
+	"github.com/018bf/creathor/internal/configs"
+)
+
+type RepositoryInterfacePermission struct {
+	project *configs.Project
+}
+
+func NewRepositoryInterfacePermission(project *configs.Project) *RepositoryInterfacePermission {
+	return &RepositoryInterfacePermission{project: project}
+}
+
+func (i RepositoryInterfacePermission) file() *ast.File {
+	return &ast.File{
+		Name: &ast.Ident{
+			Name: "repositories",
+		},
+		Decls: []ast.Decl{
+			&ast.GenDecl{
+				Tok: token.IMPORT,
+				Specs: []ast.Spec{
+					&ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: `"context"`,
+						},
+					},
+					&ast.ImportSpec{
+						Path: &ast.BasicLit{
+							Kind:  token.STRING,
+							Value: fmt.Sprintf(`"%s/internal/domain/models"`, i.project.Module),
+						},
+					},
+				},
+			},
+			&ast.GenDecl{
+				Tok: token.TYPE,
+				Doc: &ast.CommentGroup{
+					List: []*ast.Comment{
+						{
+							Text: "//PermissionRepository - domain layer repository interface",
+						},
+						{
+							Text: "//go:generate mockgen -build_flags=-mod=mod -destination mock/interfaces.go . PermissionRepository",
+						},
+					},
+				},
+				Specs: []ast.Spec{
+					&ast.TypeSpec{
+						Name: &ast.Ident{
+							Name: "PermissionRepository",
+						},
+						Type: &ast.InterfaceType{
+							Methods: &ast.FieldList{
+								List: []*ast.Field{
+									{
+										Names: []*ast.Ident{
+											{
+												Name: "HasPermission",
+											},
+										},
+										Type: &ast.FuncType{
+											Params: &ast.FieldList{
+												List: []*ast.Field{
+													{
+														Names: []*ast.Ident{
+															{
+																Name: "ctx",
+															},
+														},
+														Type: &ast.SelectorExpr{
+															X: &ast.Ident{
+																Name: "context",
+															},
+															Sel: &ast.Ident{
+																Name: "Context",
+															},
+														},
+													},
+													{
+														Names: []*ast.Ident{
+															{
+																Name: "permission",
+															},
+														},
+														Type: ast.NewIdent("uuid.UUID"),
+													},
+													{
+														Names: []*ast.Ident{
+															{
+																Name: "requestUser",
+															},
+														},
+														Type: &ast.StarExpr{
+															X: &ast.SelectorExpr{
+																X: &ast.Ident{
+																	Name: "models",
+																},
+																Sel: &ast.Ident{
+																	Name: "User",
+																},
+															},
+														},
+													},
+												},
+											},
+											Results: &ast.FieldList{
+												List: []*ast.Field{
+													{
+														Type: &ast.Ident{
+															Name: "error",
+														},
+													},
+												},
+											},
+										},
+									},
+									{
+										Names: []*ast.Ident{
+											{
+												Name: "HasObjectPermission",
+											},
+										},
+										Type: &ast.FuncType{
+											Params: &ast.FieldList{
+												List: []*ast.Field{
+													{
+														Names: []*ast.Ident{
+															{
+																Name: "ctx",
+															},
+														},
+														Type: &ast.SelectorExpr{
+															X: &ast.Ident{
+																Name: "context",
+															},
+															Sel: &ast.Ident{
+																Name: "Context",
+															},
+														},
+													},
+													{
+														Names: []*ast.Ident{
+															{
+																Name: "permission",
+															},
+														},
+														Type: ast.NewIdent("uuid.UUID"),
+													},
+													{
+														Names: []*ast.Ident{
+															{
+																Name: "user",
+															},
+														},
+														Type: &ast.StarExpr{
+															X: &ast.SelectorExpr{
+																X: &ast.Ident{
+																	Name: "models",
+																},
+																Sel: &ast.Ident{
+																	Name: "User",
+																},
+															},
+														},
+													},
+													{
+														Names: []*ast.Ident{
+															{
+																Name: "obj",
+															},
+														},
+														Type: &ast.Ident{
+															Name: "any",
+														},
+													},
+												},
+											},
+											Results: &ast.FieldList{
+												List: []*ast.Field{
+													{
+														Type: &ast.Ident{
+															Name: "error",
+														},
+													},
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+}
+
+func (i RepositoryInterfacePermission) Sync() error {
+	fileset := token.NewFileSet()
+	filename := path.Join("internal", "auth", "repositories", "permission.go")
+	file, err := parser.ParseFile(fileset, filename, nil, parser.ParseComments)
+	if err != nil {
+		file = i.file()
+	}
+	buff := &bytes.Buffer{}
+	if err := printer.Fprint(buff, fileset, file); err != nil {
+		return err
+	}
+	if err := os.WriteFile(filename, buff.Bytes(), 0777); err != nil {
+		return err
+	}
+	return nil
+}
